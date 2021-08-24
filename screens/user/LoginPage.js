849aa0030245cc565781a8d16ba776868eb717a3
NaimBiswas <nayeembiswas2@gmail.com>
2021-08-24 14:36:51 +0600

    login api implemented with message'

--- screens/user/LoginPage.js
+++ screens/user/LoginPage.js
@@ -1,17 +1,76 @@
+/* eslint-disable handle-callback-err */
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
-import { Dimensions, Image, ImageBackground, Keyboard, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react';
+import { Dimensions, Image, ImageBackground, Keyboard, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import Toast from 'react-native-toast-message';
 
-const LoginPage = () => {
-   const [email, setEmail] = useState('')
-   const [password, setPassword] = useState('')
+const LoginPage = ({ navigation }) => {
+   const [email, setEmail] = useState('');
+   const [password, setPassword] = useState('');
+
+
+
+   const loginFun = (email, password) => {
+
+      fetch('https://shop-bc.herokuapp.com/api/login', {
+         method: 'POST',
+         headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+         },
+         body: JSON.stringify({
+            email: email,
+            password: password,
+         }),
+      }).then(res => res.json())
+         .then((result) => {
+            if (result.succ === true) {
+               Toast.show({
+                  type: 'success',
+                  autoHide: true,
+                  text1: result?.message,
+                  text2: 'Thanks for being with us!',
+               });
+               navigation.navigate('Feed');
+            } else {
+               Toast.show({
+                  type: 'error',
+                  autoHide: true,
+                  text1: result?.message,
+                  text2: result?.messageTwo ? result?.messageTwo : 'Please create a account & continue',
+               });
+            }
+         }).catch(err => {
+            console.log(err);
+            Toast.show({
+               type: 'error',
+               autoHide: true,
+               text1: 'Something went wrong',
+               text2: 'Try again later',
+            });
+         });
+   };
+
+
+
+
+
+
+
+
+
+
+
+
+
    return (
       <ScrollView onPress={Keyboard.dismiss}  >
          <ImageBackground source={require('../../assets/image/bgOne.jpg')}
             style={{
                minHeight: Dimensions.get('window').height,
-               width: "100%",
-               height: "100%"
+               width: '100%',
+               height: '100%',
             }}
          >
 
@@ -40,7 +99,9 @@ const LoginPage = () => {
                </View>
 
 
-               <Pressable style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '10%', }}>
+               <Pressable
+                  onPress={() => { loginFun(email, password); }}
+                  style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '10%' }}>
                   <Text style={styles.registerButton} >Log In</Text>
                </Pressable>
             </View>
@@ -48,34 +109,34 @@ const LoginPage = () => {
          </ImageBackground>
       </ScrollView>
 
-   )
-}
+   );
+};
 
-export default LoginPage
+export default LoginPage;
 
 const styles = StyleSheet.create({
    registerButton: {
       padding: 20,
       backgroundColor: '#012840',
-      color: "#fff",
+      color: '#fff',
       fontSize: 15,
       letterSpacing: 2,
       borderRadius: 10,
    },
    inputStyle: {
-      borderBottomColor: "#fff",
+      borderBottomColor: '#fff',
       borderBottomWidth: 1,
       marginBottom: 20,
-      color: "#fff",
+      color: '#fff',
       fontSize: 16,
    },
    inputContainer: {
       margin: 20,
-      marginLeft: '10%', marginTop: '18%', marginRight: '10%'
+      marginLeft: '10%', marginTop: '18%', marginRight: '10%',
    },
    titleStyle: {
       color: '#fff',
       fontSize: 18,
 
-   }
-})
+   },
+});
